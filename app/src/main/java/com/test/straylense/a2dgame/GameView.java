f744f3af85578fd512592e831a23ad2883e8fd35
Travis Decker <decker4693@gmail.com>
2018-12-17 14:42:58 -0700

    Initial commit

---
+++ app/src/main/java/com/test/straylense/a2dgame/GameView.java
@@ -0,0 +1,65 @@
+package com.test.straylense.a2dgame;
+
+import android.content.Context;
+
+import com.test.straylense.a2dgame.MainThread;
+
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private MainThread thread;
+    private CharacterSprite characterSprite;
+
+    public GameView(Context context) {
+        super(context);
+
+        getHolder().addCallback(this);
+
+        thread = new MainThread(getHolder(), this);
+        setFocusable(true);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        characterSprite = new CharacterSprite(BitmapFactory.decodeResource(getResources(), R.drawable.ring));
+        thread.setRunning(true);
+        thread.start();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+        boolean retry = true;
+        while (retry) {
+
+            try {
+                thread.setRunning(false);
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            retry = false;
+        }
+    }
+
+    public void update() {
+      characterSprite.update();
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        if (canvas != null) {
+            characterSprite.draw(canvas);
+        }
+    }
+}
